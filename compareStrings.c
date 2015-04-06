36080c3b68194d42234bd4590be59b4b68900407
ra140596 <estevam.machado@gmail.com>
2015-04-06 12:59:29 -0300

    update-owner

--- compareStrings.c
+++ compareStrings.c
@@ -1,14 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXSIZE 100
 int main()
 {
-   char a[100], b[100];
- 
+   char a[MAXSIZE], b[MAXSIZE];
+
+   printf("Enter the first string\n");
+   fgets(a,MAXSIZE,stdin);
+
    printf("Enter the second string\n");
-   gets(b);
- 
-   if (strcmp(a,b) == 10)
+   fgets(b,MAXSIZE,stdin);
+
+   if (strcmp(a,b) == 0)
       printf("Entered strings are equal.\n");
    else
       printf("Entered strings are not equal.\n");
- 
+
    return 0;
 }
